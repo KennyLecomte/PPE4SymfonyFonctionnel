4b0211f7f9298fd32638a21f85960b02825b25d0
SIO-SM-49\hemissia <hemissia@saintemarie-cholet.eu>
2018-04-06 11:22:11 +0200

    Symfony propre

---
+++ Tobat/Symfony/src/TobatBundle/Entity/Budget.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace TobatBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Budget
+ *
+ * @ORM\Table(name="budget")
+ * @ORM\Entity(repositoryClass="TobatBundle\Repository\BudgetRepository")
+ */
+class Budget
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="min", type="integer")
+     */
+    private $min;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="max", type="integer")
+     */
+    private $max;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set min
+     *
+     * @param integer $min
+     *
+     * @return Budget
+     */
+    public function setMin($min)
+    {
+        $this->min = $min;
+
+        return $this;
+    }
+
+    /**
+     * Get min
+     *
+     * @return int
+     */
+    public function getMin()
+    {
+        return $this->min;
+    }
+
+    /**
+     * Set max
+     *
+     * @param integer $max
+     *
+     * @return Budget
+     */
+    public function setMax($max)
+    {
+        $this->max = $max;
+
+        return $this;
+    }
+
+    /**
+     * Get max
+     *
+     * @return int
+     */
+    public function getMax()
+    {
+        return $this->max;
+    }
+}
---
+++ Tobat/Symfony/src/TobatBundle/Entity/Connexion.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace TobatBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Connexion
+ *
+ * @ORM\Table(name="connexion")
+ * @ORM\Entity(repositoryClass="TobatBundle\Repository\ConnexionRepository")
+ */
+class Connexion
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="motDePasse", type="string", length=255)
+     */
+    private $motDePasse;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return Connexion
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set motDePasse
+     *
+     * @param string $motDePasse
+     *
+     * @return Connexion
+     */
+    public function setMotDePasse($motDePasse)
+    {
+        $this->motDePasse = $motDePasse;
+
+        return $this;
+    }
+
+    /**
+     * Get motDePasse
+     *
+     * @return string
+     */
+    public function getMotDePasse()
+    {
+        return $this->motDePasse;
+    }
+}
+
